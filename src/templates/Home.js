a4207764fc415466cc8e99101a9d20a5c920e642
Tim Holt <tim@astronautweb.co>
2017-05-04 22:18:43 +1000

    CI config changes

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 class Home extends React.Component {
   render() {
     return (
-     <main>
-       <h1>Home</h1>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quam earum dolorem quasi. Reprehenderit dicta voluptatum quaerat consectetur suscipit, soluta ullam, impedit, eos facere delectus quis vitae veritatis. In, architecto.</p>
-     </main>
-    )
+      <main>
+        <h1>Home</h1>
+        <p>
+          Hello S3. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quam earum dolorem quasi. Reprehenderit dicta voluptatum quaerat consectetur suscipit, soluta ullam, impedit, eos facere delectus quis vitae veritatis. In, architecto.
+        </p>
+      </main>
+    );
   }
 }
 
