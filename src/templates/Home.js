0d160615b67c7dcc94504b5225c1857aa7da2b70
Tim Holt <tim@astronautweb.co>
2017-05-12 22:39:46 +1000

    added WP posts

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import posts from '../posts.json';
+
+const Post = props => {
+  function content() {
+    return {
+      __html: props.content
+    };
+  }
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      <p dangerouslySetInnerHTML={content()} />
+    </article>
+  );
+};
 
 class Home extends React.Component {
   render() {
@@ -8,6 +23,15 @@ class Home extends React.Component {
         <p>
           I‘m a front end web developer at the ABC, based in Brisbane. I am currently working on a project to build a new platform for the front end with server-rendered React.
         </p>
+        {posts.map(post => {
+          return (
+            <Post
+              key={post.id}
+              title={post.title.rendered}
+              content={post.content.rendered}
+            />
+          );
+        })}
       </main>
     );
   }
