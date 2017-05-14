e5c31343b7177dd0769ec5456ff250fa2ad6e55c
Tim Holt <tim@astronautweb.co>
2017-05-14 21:14:45 +1000

    travis cloudfront invalidation package

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -5,6 +5,7 @@ class Home extends React.Component {
     return (
       <main>
         <h1>S3 and Cloudfront</h1>
+        <h2>Deployed with Travis CI</h2>
         <p>
           I‘m a front end web developer at the ABC, based in Brisbane. I am currently working on a project to build a new platform for the front end with server-rendered React.
         </p>
