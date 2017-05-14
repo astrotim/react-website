8a9a4afbd1d2c991682a2a99351d1a79e8247bb6
Tim Holt <tim@astronautweb.co>
2017-05-14 20:26:30 +1000

    updated homepage content; disabled invalidation command

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -1,37 +1,25 @@
 import React from 'react';
-import posts from '../posts.json';
-
-const Post = props => {
-  function content() {
-    return {
-      __html: props.content
-    };
-  }
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      <p dangerouslySetInnerHTML={content()} />
-    </article>
-  );
-};
 
 class Home extends React.Component {
   render() {
     return (
       <main>
-        <h1>Critical CSS</h1>
+        <h1>S3 and Cloudfront</h1>
         <p>
           I‘m a front end web developer at the ABC, based in Brisbane. I am currently working on a project to build a new platform for the front end with server-rendered React.
         </p>
-        {posts.map(post => {
-          return (
-            <Post
-              key={post.id}
-              title={post.title.rendered}
-              content={post.content.rendered}
-            />
-          );
-        })}
+        <div className="hero-wrapper">
+          <img className="hero-image" src="/assets/hero.jpg" />
+        </div>
+        <p>
+          I build high quality, handcrafted products with modern best practices.
+        </p>
+        <blockquote>
+          Tim has been an invaluable asset to the Taste team... [he is] great to work with, his service exceptional and we highly recommend working with him.
+          <footer>
+            Laura Arnold, Marketing Manager, Taste Festivals Australia
+          </footer>
+        </blockquote>
       </main>
     );
   }
